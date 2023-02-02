e72bcbe0e4296b44f089ce89842e47e9e45ac75a
Eric Julianto <algonacci1@gmail.com>
2023-02-02 18:25:44 +0700

    update and delete task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from db import db, cursor
 
 app = Flask(__name__)
@@ -16,9 +16,30 @@ def create_task():
     task = request.form["todo"]
     cursor.execute("INSERT INTO todos (`todo`) VALUES ('{}')".format(task))
     db.commit()
-    cursor.execute("SELECT * FROM todos")
+    return redirect("/")
+
+
+@app.get("/edit/<int:id>")
+def edit_task(id):
+    cursor.execute("SELECT * FROM todos WHERE id = ('{}')".format(id))
     data = cursor.fetchall()
-    return render_template("index.html", data=data)
+    return render_template("edit.html", data=data)
+
+
+@app.post("/update/<int:id>")
+def update_task(id):
+    todo = request.form["todo"]
+    cursor.execute(
+        "UPDATE todos SET todo = ('{}') WHERE ID = ('{}')".format(todo, id))
+    db.commit()
+    return redirect("/")
+
+
+@app.post("/delete/<int:id>")
+def delete_task(id):
+    cursor.execute("DELETE FROM todos WHERE id = ('{}')".format(id))
+    db.commit()
+    return redirect("/")
 
 
 if __name__ == "__main__":
