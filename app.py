fdcd3d8edf0a6976791baad4577adccbd0732ba9
Eric Julianto <algonacci1@gmail.com>
2023-02-02 17:55:07 +0700

    post task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from db import db, cursor
 
 app = Flask(__name__)
@@ -8,8 +8,17 @@ app = Flask(__name__)
 def root():
     cursor.execute("SELECT * FROM todos")
     data = cursor.fetchall()
-    print(data)
-    return "Hello World!"
+    return render_template("index.html", data=data)
+
+
+@app.post("/")
+def create_task():
+    task = request.form["todo"]
+    cursor.execute("INSERT INTO todos (`todo`) VALUES ('{}')".format(task))
+    db.commit()
+    cursor.execute("SELECT * FROM todos")
+    data = cursor.fetchall()
+    return render_template("index.html", data=data)
 
 
 if __name__ == "__main__":
